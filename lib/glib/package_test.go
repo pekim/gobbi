95c5605613d916bbdd9e530592153f1748374aca
Mike D Pilsbury <mike.pilsbury@gmail.com>
2018-09-11 18:13:32 +0100

    Add test to call a glib function; currently fails.
    Will not compile, as function signature does not yet
    include return value.

--- lib/glib/package_test.go
+++ lib/glib/package_test.go
@@ -9,3 +9,7 @@ import (
 func TestCleanBuild(t *testing.T) {
 	assert.Equal(t, AsciiType(16), ASCII_GRAPH)
 }
+
+func TestFunctionCall(t *testing.T) {
+	assert.Equal(t, 0x30, AsciiDigitValue('A'))
+}
