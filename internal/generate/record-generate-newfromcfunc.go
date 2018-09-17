f4302693b72f4146bf392f2c0bea30697208a8e2
Mike D Pilsbury <mike.pilsbury@gmail.com>
2018-09-17 18:19:30 +0100

    Move generation of newFromC func to own struct.

---
+++ internal/generate/record-generate-newfromcfunc.go
@@ -0,0 +1,108 @@
+package generate
+
+import "github.com/dave/jennifer/jen"
+
+type RecordNewFromCFunc struct {
+	*Record
+}
+
+func (r *RecordNewFromCFunc) generate(g *jen.Group) {
+	g.
+		Func().
+		Id(r.newFromCFuncName).
+		ParamsFunc(r.generateParams).
+		ParamsFunc(r.generateNewFromCFunc).
+		BlockFunc(func(g *jen.Group) {
+			r.generateNotCreateGoStruct(g)
+			g.Line()
+
+			r.generateCreateGoStruct(g)
+			g.Line()
+
+			r.generateFinalizeFree(g)
+			g.Line()
+
+			g.
+				Return().
+				Id("g")
+		})
+
+	g.Line()
+}
+
+func (r *RecordNewFromCFunc) generateParams(g *jen.Group) {
+	g.
+		Id("c").
+		Op("*").
+		Qual("C", r.CType)
+
+	g.
+		Id("finalizeFree").
+		Id("bool")
+}
+
+func (r *RecordNewFromCFunc) generateNewFromCFunc(g *jen.Group) {
+	g.
+		Op("*").
+		Id(r.GoName)
+}
+
+func (r *RecordNewFromCFunc) generateNotCreateGoStruct(g *jen.Group) {
+	// If the C parameter is nil, do not create a Go struct.
+	// Instead, return nil.
+	g.If(
+		jen.
+			Id("c").
+			Op("==").
+			Nil()).
+		Block(jen.Return(jen.Nil()))
+}
+
+func (r *RecordNewFromCFunc) generateCreateGoStruct(g *jen.Group) {
+	g.
+		Id("g").
+		Op(":=").
+		Op("&").
+		Id(r.GoName).
+		Values(
+			jen.DictFunc(func(d jen.Dict) {
+				d[jen.Id("native")] = jen.Id("c")
+
+				for _, f := range r.Fields {
+					if supported, _ := f.supported(); !supported {
+						continue
+					}
+
+					cValue := jen.
+						Id("c").
+						Op(".").
+						Id(f.Name)
+
+					d[jen.Id(f.goVarName)] = f.Type.generator.generateCToGo(cValue)
+				}
+			}))
+}
+
+func (r *RecordNewFromCFunc) generateFinalizeFree(g *jen.Group) {
+	g.
+		If(jen.Id("finalizeFree")).
+		BlockFunc(func(g *jen.Group) {
+			g.Qual("runtime", "SetFinalizer").
+				Call(
+					jen.Id("g"),
+					jen.
+						Func().
+						Params(
+							jen.
+								Id("obj").
+								Id("interface").
+								Op("{}")).
+						BlockFunc(func(g *jen.Group) {
+							g.
+								Qual("C", "g_free").
+								Call(jen.
+									Parens(jen.Qual("C", "gpointer")).
+									Parens(jen.Id("c")))
+						}))
+		})
+}
--- internal/generate/record.go
+++ internal/generate/record.go
@@ -63,7 +63,7 @@ func (r *Record) mergeAddenda(addenda *Record) {
 
 func (r *Record) generate(g *jen.Group, version *Version) {
 	r.generateType(g)
-	r.generateNewFromCFunc(g)
+	(&RecordNewFromCFunc{r}).generate(g)
 }
 
 func (r *Record) generateType(g *jen.Group) {
@@ -80,87 +80,6 @@ func (r *Record) generateType(g *jen.Group) {
 
 			r.Fields.generate(g)
 		})
-	g.Line()
-}
-
-func (r *Record) generateNewFromCFunc(g *jen.Group) {
-	g.
-		Func().
-		Id(r.newFromCFuncName).
-		Params(jen.
-			Id("c").
-			Op("*").
-			Qual("C", r.CType),
-
-			jen.
-				Id("finalizeFree").
-				Id("bool")).
-		Params(jen.
-			Op("*").
-			Id(r.GoName)).
-		BlockFunc(func(g *jen.Group) {
-			// If the C parameter is nil, do not create a Go struct.
-			g.If(
-				jen.
-					Id("c").
-					Op("==").
-					Nil()).
-				Block(jen.Return(jen.Nil()))
-			g.Line()
-
-			g.
-				Id("g").
-				Op(":=").
-				Op("&").
-				Id(r.GoName).
-				Values(
-					jen.DictFunc(func(d jen.Dict) {
-						d[jen.Id("native")] = jen.Id("c")
-
-						for _, f := range r.Fields {
-							if supported, _ := f.supported(); !supported {
-								continue
-							}
-
-							cValue := jen.
-								Id("c").
-								Op(".").
-								Id(f.Name)
-
-							d[jen.Id(f.goVarName)] = f.Type.generator.generateCToGo(cValue)
-						}
-					}))
-			g.Line()
-
-			g.
-				If(jen.Id("finalizeFree")).
-				BlockFunc(func(g *jen.Group) {
-					g.Qual("runtime", "SetFinalizer").
-						Call(
-							jen.Id("g"),
-							jen.
-								Func().
-								Params(
-									jen.
-										Id("obj").
-										Id("interface").
-										Op("{}")).
-								BlockFunc(func(g *jen.Group) {
-									g.
-										Qual("C", "g_free").
-										Call(jen.
-											Parens(jen.Qual("C", "gpointer")).
-											Parens(jen.Id("c")))
-								}))
-				})
-
-			g.Line()
-
-			g.
-				Return().
-				Id("g")
-		})
-
 	g.Line()
 }
 
