43e8f172478f0b7ec108aecc117bae6724287560
Mike D Pilsbury <mike.pilsbury@gmail.com>
2018-09-23 13:15:46 +0100

    Write generate timing messages to stdout.

--- internal/cmd/generate/main.go
+++ internal/cmd/generate/main.go
@@ -1,13 +1,29 @@
 package main
 
 import (
+	"fmt"
 	"github.com/pekim/gobbi/internal/generate"
+	"time"
 )
 
 func main() {
+	start := time.Now()
+
 	girs := generate.GirNewRoot("Gio", "2.0")
+
+	parseDuration := time.Now().Sub(start)
+	start = time.Now()
 
 	for _, gir := range girs {
 		gir.Generate()
 	}
+
+	generateDuration := time.Now().Sub(start)
+
+	durationMessage("parse", parseDuration)
+	durationMessage("generate", generateDuration)
+}
+
+func durationMessage(action string, duration time.Duration) {
+	fmt.Printf("  %-8s %.0fms\n", action, duration.Seconds()*1000)
 }
