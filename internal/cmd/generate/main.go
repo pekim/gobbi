f3e5d6baae4ca2cc9ba91dd5baf8081971be2a5b
Mike D Pilsbury <mike.pilsbury@gmail.com>
2018-09-29 09:06:59 +0100

    Output line stats for generated code.

--- internal/cmd/generate/main.go
+++ internal/cmd/generate/main.go
@@ -2,13 +2,25 @@ package main
 
 import (
 	"fmt"
+	"github.com/boyter/scc/processor"
 	"github.com/pekim/gobbi/internal/generate"
 	"time"
 )
 
 func main() {
+	generateLibraries()
+	outputScc()
+}
+
+func generateLibraries() {
 	start := time.Now()
 	generate.FromRoot("Gtk", "3.0")
 	fmt.Printf("\ngeneration %.0fms\n\n", time.Now().Sub(start).Seconds()*1000)
+}
 
+func outputScc() {
+	processor.DirFilePaths = []string{"./lib/"}
+	processor.PathBlacklist = ".git"
+	processor.Cocomo = true
+	processor.Process()
 }
