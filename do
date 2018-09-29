2ae0850b8ed2abc865170e23f611dd7a4b7f9d90
Mike D Pilsbury <mike.pilsbury@gmail.com>
2018-09-29 11:15:44 +0100

    Move source code stats output to a separate file.

--- do
+++ do
@@ -29,7 +29,7 @@ build () {
 }
 
 generate () {
-    go run internal/cmd/generate/main.go
+    go run internal/cmd/generate/*.go
 }
 
 test () {
--- internal/cmd/generate/main.go
+++ internal/cmd/generate/main.go
@@ -2,12 +2,7 @@ package main
 
 import (
 	"fmt"
-	"github.com/boyter/scc/processor"
 	"github.com/pekim/gobbi/internal/generate"
-	"io/ioutil"
-	"os"
-	"path/filepath"
-	"strings"
 	"time"
 )
 
@@ -19,36 +14,7 @@ func main() {
 func generateLibraries() {
 	start := time.Now()
 	generate.FromRoot("Gtk", "3.0")
-	fmt.Printf("\ngeneration %.0fms\n\n", time.Now().Sub(start).Seconds()*1000)
-}
+	end := time.Now()
 
-func outputScc() {
-	processor.ProcessConstants()
-
-	var fileCount int
-	var allFiles processor.FileJob
-
-	filepath.Walk("./lib", func(path string, info os.FileInfo, err error) error {
-		if info.IsDir() || !strings.HasSuffix(path, ".go") {
-			return nil
-		}
-
-		content, _ := ioutil.ReadFile(path)
-		filejob := processor.FileJob{
-			Language: "Go",
-			Content:  content,
-		}
-		processor.CountStats(&filejob)
-
-		allFiles.Blank += filejob.Blank
-		allFiles.Code += filejob.Code
-		allFiles.Comment += filejob.Comment
-		allFiles.Lines += filejob.Lines
-		fileCount++
-
-		return nil
-	})
-
-	fmt.Printf("%9s %9s %9s %9s %9s\n", "files", "lines", "code", "comment", "blank")
-	fmt.Printf("%9d %9d %9d %9d %9d\n", fileCount, allFiles.Lines, allFiles.Code, allFiles.Comment, allFiles.Blank)
+	fmt.Printf("\ngeneration %.0fms\n\n", end.Sub(start).Seconds()*1000)
 }
