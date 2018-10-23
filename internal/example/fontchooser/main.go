f6359f6edd810b01489af1fca9913671d77aaf46
Mike D Pilsbury <mike.pilsbury@gmail.com>
2018-10-23 17:55:42 +0100

    Create an example to help show interface use.

---
+++ internal/example/fontchooser/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"github.com/pekim/gobbi/lib/gtk"
+	"os"
+	"runtime"
+)
+
+func init() {
+	runtime.LockOSThread()
+}
+
+func main() {
+	gtk.Init(os.Args)
+
+	window := gtk.WindowNew(gtk.GTK_WINDOW_TOPLEVEL)
+	window.SetTitle("A window title")
+	window.SetDefaultSize(300, 300)
+
+	fontChooserButton := gtk.FontButtonNew()
+	fontChooserButton.Widget().SetHalign(gtk.GTK_ALIGN_CENTER)
+
+	// Use the FontChooser interface implemented by the button.
+	fontChooser := gtk.FontChooserNewFromC(fontChooserButton.ToC())
+
+	printFont := func() {
+		fmt.Println(fontChooserButton.GetFontName())
+		fmt.Println("  ", fontChooser.GetFontDesc().GetFamily())
+	}
+
+	printFont()
+	fontChooserButton.ConnectFontSet(printFont)
+
+	container := gtk.BoxNew(gtk.GTK_ORIENTATION_VERTICAL, 10).Container()
+	container.Add(fontChooserButton.Widget())
+	window.Container().Add(container.Widget())
+
+	window.Widget().ConnectDestroy(gtk.MainQuit)
+	window.Widget().ShowAll()
+
+	gtk.Main()
+}
