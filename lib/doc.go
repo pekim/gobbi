d24dbdb9c1474339519c800b455ee516edb22ae6
Mike D Pilsbury <mike.pilsbury@gmail.com>
2018-10-23 06:17:20 +0100

    Add a doc note about NewFromC and ToC functions.

--- lib/doc.go
+++ lib/doc.go
@@ -1,6 +1,12 @@
 /*
 Package lib is the parent package for all of the packages
 that provide APIs for the various gobject librararies.
+
+NewFromC and ToC functions
+
+A lot of objects have NewFromC and ToC functions.
+They are primarily intended for internal (sometimes cross-package) use by gobbi.
+
 */
 
 package lib
