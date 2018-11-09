53df3959ac450997b30a067944d1b4c9121f1fe2
Mike D Pilsbury <mike.pilsbury@gmail.com>
2018-11-09 14:28:44 +0000

    Make list example a little more readable.

--- example/list/main.go
+++ example/list/main.go
@@ -11,6 +11,12 @@ func init() {
 	runtime.LockOSThread()
 }
 
+const (
+	COL_NUMBER = iota
+	COL_TEXT
+	COL_COLOUR
+)
+
 func main() {
 	gtk.Init(os.Args)
 
@@ -18,60 +24,11 @@ func main() {
 	window.SetTitle("A window title")
 	window.SetDefaultSize(300, 300)
 
-	container := gtk.BoxNew(gtk.GTK_ORIENTATION_VERTICAL, 10).Container()
+	container := gtk.BoxNew(gtk.GTK_ORIENTATION_VERTICAL, 20).Container()
+	container.Widget().SetMarginStart(20)
+	container.Widget().SetMarginEnd(20)
 	container.Add(gtk.LabelNew("A tree view").Widget())
-
-	listStore := gtk.ListStoreNewv([]gobject.Type{gobject.TYPE_INT, gobject.TYPE_STRING, gobject.TYPE_STRING})
-	iter := listStore.Append()
-
-	value0 := gobject.ValueNew(gobject.TYPE_INT)
-	value0.SetInt(42)
-	listStore.SetValue(iter, 0, value0)
-
-	value1 := gobject.ValueNew(gobject.TYPE_STRING)
-	value1.SetString("qaz")
-	listStore.SetValue(iter, 1, value1)
-
-	value2 := gobject.ValueNew(gobject.TYPE_STRING)
-	value2.SetString("Red")
-	listStore.SetValue(iter, 2, value2)
-
-	iter = listStore.Append()
-
-	value01 := gobject.ValueNew(gobject.TYPE_INT)
-	value01.SetInt(43)
-	listStore.SetValue(iter, 0, value01)
-
-	value11 := gobject.ValueNew(gobject.TYPE_STRING)
-	value11.SetString("qwerty")
-	listStore.SetValue(iter, 1, value11)
-
-	value21 := gobject.ValueNew(gobject.TYPE_STRING)
-	value21.SetString("green")
-	listStore.SetValue(iter, 2, value21)
-
-	tree := gtk.TreeViewNewWithModel(listStore.TreeModel())
-
-	column0 := gtk.TreeViewColumnNew()
-	column0.SetResizable(true)
-	column0.SetTitle("integer")
-	tree.AppendColumn(column0)
-
-	renderer0 := gtk.CellRendererTextNew()
-	column0.PackStart(renderer0.CellRenderer(), true)
-	column0.AddAttribute(renderer0.CellRenderer(), "text", 0)
-
-	column1 := gtk.TreeViewColumnNew()
-	column1.SetResizable(true)
-	column1.SetTitle("text")
-	tree.AppendColumn(column1)
-
-	renderer1 := gtk.CellRendererTextNew()
-	column1.PackStart(renderer1.CellRenderer(), true)
-	column1.AddAttribute(renderer1.CellRenderer(), "text", 1)
-	column1.AddAttribute(renderer1.CellRenderer(), "foreground", 2)
-
-	container.Add(tree.Widget())
+	container.Add(createTree().Widget())
 
 	window.Container().Add(container.Widget())
 
@@ -79,4 +36,52 @@ func main() {
 	window.Widget().ShowAll()
 
 	gtk.Main()
+}
+
+func createTree() *gtk.TreeView {
+	listStore := gtk.ListStoreNewv([]gobject.Type{gobject.TYPE_INT, gobject.TYPE_STRING, gobject.TYPE_STRING})
+	addRow(listStore, 42, "some text")
+	addRow(listStore, 43, "some more text")
+	addRow(listStore, 43, "yet more text, don't you know")
+	addRow(listStore, 107, "qaz")
+	addRow(listStore, 109, "qaz qwerty")
+
+	tree := gtk.TreeViewNewWithModel(listStore.TreeModel())
+
+	addColumn(tree, "number", COL_NUMBER, -1)
+	addColumn(tree, "text", COL_TEXT, COL_COLOUR)
+
+	return tree
+}
+
+func addRow(store *gtk.ListStore, number int32, text string) {
+	iter := store.Append()
+
+	valueNumber := gobject.ValueNew(gobject.TYPE_INT)
+	valueNumber.SetInt(number)
+	store.SetValue(iter, COL_NUMBER, valueNumber)
+
+	valueText := gobject.ValueNew(gobject.TYPE_STRING)
+	valueText.SetString(text)
+	store.SetValue(iter, COL_TEXT, valueText)
+
+	valueColour := gobject.ValueNew(gobject.TYPE_STRING)
+	valueColour.SetString("red")
+	store.SetValue(iter, COL_COLOUR, valueColour)
+}
+
+func addColumn(view *gtk.TreeView, title string, modelColumn int32, foregroundColumn int32) {
+	column := gtk.TreeViewColumnNew()
+	renderer := gtk.CellRendererTextNew()
+
+	column.SetResizable(true)
+	column.SetTitle(title)
+	column.PackStart(renderer.CellRenderer(), true)
+	column.AddAttribute(renderer.CellRenderer(), "text", modelColumn)
+
+	if foregroundColumn >= 0 {
+		column.AddAttribute(renderer.CellRenderer(), "foreground", foregroundColumn)
+	}
+
+	view.AppendColumn(column)
 }
