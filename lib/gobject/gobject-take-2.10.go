237fb2034f8c360454429e9966248646f38e9ec8
Mike D Pilsbury <mike.pilsbury@gmail.com>
2018-12-06 06:32:17 +0000

    Remove "generated" comment from non-generated files.

--- lib/gobject/gobject-take-2.10.go
+++ lib/gobject/gobject-take-2.10.go
@@ -1,4 +1,3 @@
-// This is a generated file - DO NOT EDIT
 // +build gobject_2.10 gobject_2.12 gobject_2.18 gobject_2.24 gobject_2.26 gobject_2.28 gobject_2.30 gobject_2.32 gobject_2.34 gobject_2.36 gobject_2.38 gobject_2.42 gobject_2.44 gobject_2.46 gobject_2.54
 
 package gobject
