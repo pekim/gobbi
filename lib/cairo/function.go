9db54396f6cc04a4a7711f06e8e7d9334bbfb366
Mike D Pilsbury <mike.pilsbury@gmail.com>
2019-01-06 20:32:09 +0000

    Add generated file with a single blacklisted func.
    Would be nice not to have the file at all, but it
    would be silly to make a special exception for it.

---
+++ lib/cairo/function.go
@@ -0,0 +1,13 @@
+// This is a generated file - DO NOT EDIT
+
+package cairo
+
+// #cgo CFLAGS: -Wno-deprecated-declarations
+// #cgo CFLAGS: -Wno-format-security
+// #cgo CFLAGS: -Wno-incompatible-pointer-types
+// #include <glib/gstdio.h>
+// #include <cairo/cairo.h>
+// #include <stdlib.h>
+import "C"
+
+// Blacklisted : cairo_image_surface_create
