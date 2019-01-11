37e5ed5d48cba9eaed655fab3ca8d2a63489780a
Mike D Pilsbury <mike.pilsbury@gmail.com>
2019-01-11 18:08:39 +0000

    Add more cairo font face functions.

--- lib/cairo/manual_font-face-1.0.go
+++ lib/cairo/manual_font-face-1.0.go
@@ -17,3 +17,25 @@ func (ff *FontFace) destroy() {
 	c_ff := (*C.cairo_font_face_t)(ff.ToC())
 	C.cairo_font_face_destroy(c_ff)
 }
+
+func (ff *FontFace) Status() Status {
+	c_ff := (*C.cairo_font_face_t)(ff.ToC())
+
+	retC := C.cairo_font_face_status(c_ff)
+	return Status(retC)
+}
+
+func (ff *FontFace) GetType() FontType {
+	c_ff := (*C.cairo_font_face_t)(ff.ToC())
+
+	retC := C.cairo_font_face_get_type(c_ff)
+	return FontType(retC)
+}
+
+// Gobbi takes care of the referencing.
+// So this function should not be needed by applications.
+//
+// unsigned int	cairo_font_face_get_reference_count ()
+
+// cairo_status_t	cairo_font_face_set_user_data ()
+// void *	cairo_font_face_get_user_data ()
