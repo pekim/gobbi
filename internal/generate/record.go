a1118ce3bbdfd633c087e83985abf59ea5be25cf
Mike D Pilsbury <mike.pilsbury@gmail.com>
2019-06-04 17:46:17 +0100

    Initialise records with their class objects.

--- internal/generate/record.go
+++ internal/generate/record.go
@@ -11,22 +11,24 @@ type Record struct {
 	Blacklist     bool `xml:"blacklist,attr"`
 	FieldsPrivate bool `xml:"fieldsPrivate,attr"`
 
-	Name           string       `xml:"name,attr"`
-	GoName         string       `xml:"goname,attr"`
-	Version        string       `xml:"version,attr"`
-	CSymbolPrefix  string       `xml:"http://www.gtk.org/introspection/c/1.0 symbol-prefix,attr"`
-	CType          string       `xml:"http://www.gtk.org/introspection/c/1.0 type,attr"`
-	ParentName     string       `xml:"parent,attr"`
-	GlibTypeName   string       `xml:"http://www.gtk.org/introspection/glib/1.0 type-name,attr"`
-	GlibGetType    string       `xml:"http://www.gtk.org/introspection/glib/1.0 get-type,attr"`
-	GlibTypeStruct string       `xml:"http://www.gtk.org/introspection/glib/1.0 type-struct,attr"`
-	Doc            *Doc         `xml:"doc"`
-	Fields         Fields       `xml:"field"`
-	Constructors   Constructors `xml:"constructor"`
-	Functions      Functions    `xml:"function"`
-	Methods        Methods      `xml:"method"`
-	Signals        Signals      `xml:"http://www.gtk.org/introspection/glib/1.0 signal"`
+	Name             string       `xml:"name,attr"`
+	GoName           string       `xml:"goname,attr"`
+	Version          string       `xml:"version,attr"`
+	CSymbolPrefix    string       `xml:"http://www.gtk.org/introspection/c/1.0 symbol-prefix,attr"`
+	CType            string       `xml:"http://www.gtk.org/introspection/c/1.0 type,attr"`
+	IsGtypeStructFor string       `xml:"http://www.gtk.org/introspection/glib/1.0 is-gtype-struct-for,attr"`
+	ParentName       string       `xml:"parent,attr"`
+	GlibTypeName     string       `xml:"http://www.gtk.org/introspection/glib/1.0 type-name,attr"`
+	GlibGetType      string       `xml:"http://www.gtk.org/introspection/glib/1.0 get-type,attr"`
+	GlibTypeStruct   string       `xml:"http://www.gtk.org/introspection/glib/1.0 type-struct,attr"`
+	Doc              *Doc         `xml:"doc"`
+	Fields           Fields       `xml:"field"`
+	Constructors     Constructors `xml:"constructor"`
+	Functions        Functions    `xml:"function"`
+	Methods          Methods      `xml:"method"`
+	Signals          Signals      `xml:"http://www.gtk.org/introspection/glib/1.0 signal"`
 
+	gtypeStruct      *Record
 	newFromCFuncName string
 }
 
@@ -40,6 +42,20 @@ func (r *Record) init(ns *Namespace) {
 	r.Methods.init(ns, r)
 	r.Fields.init(ns)
 	r.Signals.init(ns, r)
+
+	if r.IsGtypeStructFor != "" {
+		forRecord, ok := r.Namespace.recordOrClassRecordForName(r.IsGtypeStructFor)
+		if ok {
+			forRecord.gtypeStruct = r
+		} else {
+			forInterface, ok := r.Namespace.interfaceForName(r.IsGtypeStructFor)
+			if ok {
+				forInterface.gtypeStruct = r
+			} else {
+				panic(fmt.Sprintf("No gtype struct %s found for %s", r.IsGtypeStructFor, r.Name))
+			}
+		}
+	}
 }
 
 func (r *Record) version() string {
