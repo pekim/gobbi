a9a96e4398abf3b5f7808b26a02bbfe1e90078aa
Mike D Pilsbury <mike.pilsbury@gmail.com>
2019-06-07 18:50:06 +0100

    Sort of render, and override virtual func.

--- lib/gtk/subclass.go
+++ lib/gtk/subclass.go
@@ -18,10 +18,39 @@ void entry_vf_move_cursor(GtkEntry       *entry,
 }
 
 void
-entry_vf_realize        (GtkEntry        *widget)
+entry_vf_realize        (GtkWidget        *widget)
 {
-  //GTK_WIDGET_SET_FLAGS (widget, GTK_REALIZED);
 	gtk_widget_set_realized(widget, TRUE);
+
+  GdkWindowAttr attributes;
+  gint attributes_mask;
+
+  //GtkWidget *widget;
+  //widget = (GtkWidget*) widget;
+
+  attributes.window_type = GDK_WINDOW_CHILD;
+  //attributes.x = widget->allocation.x;
+  //attributes.y = widget->allocation.y;
+  //attributes.width = widget->allocation.width;
+  //attributes.height = widget->allocation.height;
+  attributes.x = 20;
+  attributes.y = 20;
+  attributes.width = 50;
+  attributes.height = 25;
+  attributes.wclass = GDK_INPUT_OUTPUT;
+  attributes.visual = gtk_widget_get_visual (widget);
+  //attributes.colormap = gtk_widget_get_colormap (widget);
+  attributes.event_mask = gtk_widget_get_events (widget) | GDK_EXPOSURE_MASK;
+
+  attributes_mask = GDK_WA_X | GDK_WA_Y | GDK_WA_VISUAL;
+
+  //widget->window = gdk_window_new (gtk_widget_get_parent_window (widget),
+  //                                 &attributes, attributes_mask);
+  gtk_widget_set_window(widget, gdk_window_new (gtk_widget_get_parent_window (widget),
+                                   &attributes, attributes_mask));
+
+  printf("realize\n");
+  //gdk_window_set_user_data (gtk_widget_get_window(widget), widget);
 }
 
 void widget_class_init(GtkEntryClass *g_class, gpointer class_data) {
@@ -72,7 +101,7 @@ func EntryDerive(name string) *EntryDerivedClass {
 	cTypeName := C.CString(name)
 	defer C.free(unsafe.Pointer(cTypeName))
 
-	gtype := C.g_type_register_static(C.GTK_TYPE_WIDGET, cTypeName, &typeInfo, 0)
+	gtype := C.g_type_register_static(C.GTK_TYPE_ENTRY, cTypeName, &typeInfo, 0)
 
 	class := &EntryDerivedClass{
 		name:  name,
