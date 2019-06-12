cd3677ce4db6753ace4597f6a76283a70d89323a
Mike D Pilsbury <mike.pilsbury@gmail.com>
2019-06-12 07:14:01 +0100

    Use a struct instead of dodgy pointer maths.

--- example/subclass-drawingarea/sc/subclass.c
+++ example/subclass-drawingarea/sc/subclass.c
@@ -1,7 +1,9 @@
 #include <gtk/gtk.h>
 #include "_cgo_export.h"
 
+// virtual function implementation
 gboolean drawing_area_vf_draw(GtkWidget *widget, cairo_t *cr) {
+    // call Go function
 	return DrawingAreaDraw(widget, cr);
 }
 
@@ -9,5 +11,6 @@ void drawing_area_class_init(GtkDrawingAreaClass *g_class, gpointer class_data) 
 	GtkWidgetClass *widget_class;
 	widget_class = (GtkWidgetClass*) g_class;
 
+    // override virtual functions
 	widget_class->draw = drawing_area_vf_draw;
 }
