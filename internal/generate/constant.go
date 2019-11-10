9be5bbf954e8957706b011986bf7432c7789e9ad
Mike D Pilsbury <mike.pilsbury@gmail.com>
2019-11-10 12:14:03 +0000

    Generate integer constants.

--- internal/generate/constant.go
+++ internal/generate/constant.go
@@ -3,15 +3,81 @@ package generate
 // #include <gobject-introspection-1.0/girepository.h>
 import "C"
 
-import ()
+import (
+	"github.com/dave/jennifer/jen"
+	"unsafe"
+)
 
 func (r *repository) generateConstant(info *C.GIBaseInfo) {
-	//cName := C.g_base_info_get_name(info)
-	//name := C.GoString(cName)
-	//
-	//constant := (*C.GIConstantInfo)(info)
-	//var value C.GIArgument
-	//size := C.g_constant_info_get_value(constant, &value)
-	//
-	//fmt.Println(name, size)
+	cName := C.g_base_info_get_name(info)
+	name := C.GoString(cName)
+
+	typ := C.g_constant_info_get_type(info)
+	typeTag := C.g_type_info_get_tag(typ)
+
+	constant := (*C.GIConstantInfo)(info)
+	var value C.GIArgument
+	ptrValue := unsafe.Pointer(&value)
+	C.g_constant_info_get_value(constant, &value)
+
+	var jenValue *jen.Statement
+	var typeTagName string
+
+	switch typeTag {
+	case C.GI_TYPE_TAG_VOID:
+		typeTagName = "void"
+	case C.GI_TYPE_TAG_BOOLEAN:
+		typeTagName = "boolean"
+	case C.GI_TYPE_TAG_INT8:
+		jenValue = jen.Lit(int8(*((*C.gint8)(ptrValue))))
+	case C.GI_TYPE_TAG_UINT8:
+		jenValue = jen.Lit(uint8(*((*C.guint8)(ptrValue))))
+	case C.GI_TYPE_TAG_INT16:
+		jenValue = jen.Lit(int16(*((*C.gint16)(ptrValue))))
+	case C.GI_TYPE_TAG_UINT16:
+		jenValue = jen.Lit(uint16(*((*C.guint16)(ptrValue))))
+	case C.GI_TYPE_TAG_INT32:
+		jenValue = jen.Lit(int32(*((*C.gint32)(ptrValue))))
+	case C.GI_TYPE_TAG_UINT32:
+		jenValue = jen.Lit(uint32(*((*C.guint32)(ptrValue))))
+	case C.GI_TYPE_TAG_INT64:
+		jenValue = jen.Lit(int64(*((*C.gint64)(ptrValue))))
+	case C.GI_TYPE_TAG_UINT64:
+		jenValue = jen.Lit(uint64(*((*C.guint64)(ptrValue))))
+	case C.GI_TYPE_TAG_FLOAT:
+		typeTagName = "float"
+	case C.GI_TYPE_TAG_DOUBLE:
+		typeTagName = "double floating point"
+	case C.GI_TYPE_TAG_GTYPE:
+		typeTagName = "a GType"
+	case C.GI_TYPE_TAG_UTF8:
+		typeTagName = "a UTF-8 encoded string"
+	case C.GI_TYPE_TAG_FILENAME:
+		typeTagName = "a filename, encoded in the same encoding as the native filesystem is using."
+	case C.GI_TYPE_TAG_ARRAY:
+		typeTagName = "an array"
+	case C.GI_TYPE_TAG_INTERFACE:
+		typeTagName = "an extended interface object"
+	case C.GI_TYPE_TAG_GLIST:
+		typeTagName = " GList"
+	case C.GI_TYPE_TAG_GSLIST:
+		typeTagName = " GSList"
+	case C.GI_TYPE_TAG_GHASH:
+		typeTagName = " GHashTable"
+	case C.GI_TYPE_TAG_ERROR:
+		typeTagName = " GError"
+	case C.GI_TYPE_TAG_UNICHAR:
+		typeTagName = "Unicode character"
+	}
+
+	if jenValue == nil {
+		r.file.Commentf("Unsupported constant type '%s' (%d) for %s", typeTagName, typeTag, name)
+		return
+	}
+
+	r.file.
+		Const().
+		Id(name).
+		Op("=").
+		Add(jenValue)
 }
--- lib/glib/constant.go.go
+++ lib/glib/constant.go.go
@@ -1,3 +1,144 @@
 // Code generated - DO NOT EDIT.
 
 package glib
+
+const ANALYZER_ANALYZING = int32(1)
+const ASCII_DTOSTR_BUF_SIZE = int32(39)
+const BIG_ENDIAN = int32(4321)
+
+// Unsupported constant type 'a UTF-8 encoded string' (13) for CSET_A_2_Z
+// Unsupported constant type 'a UTF-8 encoded string' (13) for CSET_DIGITS
+// Unsupported constant type 'a UTF-8 encoded string' (13) for CSET_a_2_z
+const DATALIST_FLAGS_MASK = int32(3)
+const DATE_BAD_DAY = int32(0)
+const DATE_BAD_JULIAN = int32(0)
+const DATE_BAD_YEAR = int32(0)
+const DIR_SEPARATOR = int32(47)
+
+// Unsupported constant type 'a UTF-8 encoded string' (13) for DIR_SEPARATOR_S
+// Unsupported constant type 'double floating point' (11) for E
+// Unsupported constant type 'a UTF-8 encoded string' (13) for GINT16_FORMAT
+// Unsupported constant type 'a UTF-8 encoded string' (13) for GINT16_MODIFIER
+// Unsupported constant type 'a UTF-8 encoded string' (13) for GINT32_FORMAT
+// Unsupported constant type 'a UTF-8 encoded string' (13) for GINT32_MODIFIER
+// Unsupported constant type 'a UTF-8 encoded string' (13) for GINT64_FORMAT
+// Unsupported constant type 'a UTF-8 encoded string' (13) for GINT64_MODIFIER
+// Unsupported constant type 'a UTF-8 encoded string' (13) for GINTPTR_FORMAT
+// Unsupported constant type 'a UTF-8 encoded string' (13) for GINTPTR_MODIFIER
+// Unsupported constant type 'a UTF-8 encoded string' (13) for GNUC_FUNCTION
+// Unsupported constant type 'a UTF-8 encoded string' (13) for GNUC_PRETTY_FUNCTION
+// Unsupported constant type 'a UTF-8 encoded string' (13) for GSIZE_FORMAT
+// Unsupported constant type 'a UTF-8 encoded string' (13) for GSIZE_MODIFIER
+// Unsupported constant type 'a UTF-8 encoded string' (13) for GSSIZE_FORMAT
+// Unsupported constant type 'a UTF-8 encoded string' (13) for GSSIZE_MODIFIER
+// Unsupported constant type 'a UTF-8 encoded string' (13) for GUINT16_FORMAT
+// Unsupported constant type 'a UTF-8 encoded string' (13) for GUINT32_FORMAT
+// Unsupported constant type 'a UTF-8 encoded string' (13) for GUINT64_FORMAT
+// Unsupported constant type 'a UTF-8 encoded string' (13) for GUINTPTR_FORMAT
+const HAVE_GINT64 = int32(1)
+const HAVE_GNUC_VARARGS = int32(1)
+const HAVE_GNUC_VISIBILITY = int32(1)
+const HAVE_GROWING_STACK = int32(0)
+const HAVE_ISO_VARARGS = int32(1)
+const HOOK_FLAG_USER_SHIFT = int32(4)
+const IEEE754_DOUBLE_BIAS = int32(1023)
+const IEEE754_FLOAT_BIAS = int32(127)
+
+// Unsupported constant type 'a UTF-8 encoded string' (13) for KEY_FILE_DESKTOP_ACTION_GROUP_PREFIX
+// Unsupported constant type 'a UTF-8 encoded string' (13) for KEY_FILE_DESKTOP_GROUP
+// Unsupported constant type 'a UTF-8 encoded string' (13) for KEY_FILE_DESKTOP_KEY_ACTIONS
+// Unsupported constant type 'a UTF-8 encoded string' (13) for KEY_FILE_DESKTOP_KEY_CATEGORIES
+// Unsupported constant type 'a UTF-8 encoded string' (13) for KEY_FILE_DESKTOP_KEY_COMMENT
+// Unsupported constant type 'a UTF-8 encoded string' (13) for KEY_FILE_DESKTOP_KEY_DBUS_ACTIVATABLE
+// Unsupported constant type 'a UTF-8 encoded string' (13) for KEY_FILE_DESKTOP_KEY_EXEC
+// Unsupported constant type 'a UTF-8 encoded string' (13) for KEY_FILE_DESKTOP_KEY_FULLNAME
+// Unsupported constant type 'a UTF-8 encoded string' (13) for KEY_FILE_DESKTOP_KEY_GENERIC_NAME
+// Unsupported constant type 'a UTF-8 encoded string' (13) for KEY_FILE_DESKTOP_KEY_GETTEXT_DOMAIN
+// Unsupported constant type 'a UTF-8 encoded string' (13) for KEY_FILE_DESKTOP_KEY_HIDDEN
+// Unsupported constant type 'a UTF-8 encoded string' (13) for KEY_FILE_DESKTOP_KEY_ICON
+// Unsupported constant type 'a UTF-8 encoded string' (13) for KEY_FILE_DESKTOP_KEY_KEYWORDS
+// Unsupported constant type 'a UTF-8 encoded string' (13) for KEY_FILE_DESKTOP_KEY_MIME_TYPE
+// Unsupported constant type 'a UTF-8 encoded string' (13) for KEY_FILE_DESKTOP_KEY_NAME
+// Unsupported constant type 'a UTF-8 encoded string' (13) for KEY_FILE_DESKTOP_KEY_NOT_SHOW_IN
+// Unsupported constant type 'a UTF-8 encoded string' (13) for KEY_FILE_DESKTOP_KEY_NO_DISPLAY
+// Unsupported constant type 'a UTF-8 encoded string' (13) for KEY_FILE_DESKTOP_KEY_ONLY_SHOW_IN
+// Unsupported constant type 'a UTF-8 encoded string' (13) for KEY_FILE_DESKTOP_KEY_PATH
+// Unsupported constant type 'a UTF-8 encoded string' (13) for KEY_FILE_DESKTOP_KEY_STARTUP_NOTIFY
+// Unsupported constant type 'a UTF-8 encoded string' (13) for KEY_FILE_DESKTOP_KEY_STARTUP_WM_CLASS
+// Unsupported constant type 'a UTF-8 encoded string' (13) for KEY_FILE_DESKTOP_KEY_TERMINAL
+// Unsupported constant type 'a UTF-8 encoded string' (13) for KEY_FILE_DESKTOP_KEY_TRY_EXEC
+// Unsupported constant type 'a UTF-8 encoded string' (13) for KEY_FILE_DESKTOP_KEY_TYPE
+// Unsupported constant type 'a UTF-8 encoded string' (13) for KEY_FILE_DESKTOP_KEY_URL
+// Unsupported constant type 'a UTF-8 encoded string' (13) for KEY_FILE_DESKTOP_KEY_VERSION
+// Unsupported constant type 'a UTF-8 encoded string' (13) for KEY_FILE_DESKTOP_TYPE_APPLICATION
+// Unsupported constant type 'a UTF-8 encoded string' (13) for KEY_FILE_DESKTOP_TYPE_DIRECTORY
+// Unsupported constant type 'a UTF-8 encoded string' (13) for KEY_FILE_DESKTOP_TYPE_LINK
+const LITTLE_ENDIAN = int32(1234)
+
+// Unsupported constant type 'double floating point' (11) for LN10
+// Unsupported constant type 'double floating point' (11) for LN2
+// Unsupported constant type 'double floating point' (11) for LOG_2_BASE_10
+const LOG_DOMAIN = int8(0)
+const LOG_FATAL_MASK = int32(5)
+const LOG_LEVEL_USER_SHIFT = int32(8)
+const MAJOR_VERSION = int32(2)
+const MAXINT16 = int16(32767)
+const MAXINT32 = int32(2147483647)
+const MAXINT64 = int64(9223372036854775807)
+const MAXINT8 = int8(127)
+const MAXUINT16 = uint16(0xffff)
+const MAXUINT32 = uint32(0xffffffff)
+const MAXUINT64 = uint64(0xffffffffffffffff)
+const MAXUINT8 = uint8(0xff)
+const MICRO_VERSION = int32(1)
+const MININT16 = int16(-32768)
+const MININT32 = int32(-2147483648)
+const MININT64 = int64(-9223372036854775808)
+const MININT8 = int8(-128)
+const MINOR_VERSION = int32(56)
+
+// Unsupported constant type 'a UTF-8 encoded string' (13) for MODULE_SUFFIX
+// Unsupported constant type 'a UTF-8 encoded string' (13) for OPTION_REMAINING
+const PDP_ENDIAN = int32(3412)
+
+// Unsupported constant type 'double floating point' (11) for PI
+// Unsupported constant type 'a UTF-8 encoded string' (13) for PID_FORMAT
+// Unsupported constant type 'double floating point' (11) for PI_2
+// Unsupported constant type 'double floating point' (11) for PI_4
+// Unsupported constant type 'a UTF-8 encoded string' (13) for POLLFD_FORMAT
+const PRIORITY_DEFAULT = int32(0)
+const PRIORITY_DEFAULT_IDLE = int32(200)
+const PRIORITY_HIGH = int32(-100)
+const PRIORITY_HIGH_IDLE = int32(100)
+const PRIORITY_LOW = int32(300)
+const SEARCHPATH_SEPARATOR = int32(58)
+
+// Unsupported constant type 'a UTF-8 encoded string' (13) for SEARCHPATH_SEPARATOR_S
+const SIZEOF_LONG = int32(8)
+const SIZEOF_SIZE_T = int32(8)
+const SIZEOF_SSIZE_T = int32(8)
+const SIZEOF_VOID_P = int32(8)
+
+// Unsupported constant type 'boolean' (1) for SOURCE_CONTINUE
+// Unsupported constant type 'boolean' (1) for SOURCE_REMOVE
+// Unsupported constant type 'double floating point' (11) for SQRT2
+// Unsupported constant type 'a UTF-8 encoded string' (13) for STR_DELIMITERS
+const SYSDEF_AF_INET = int32(2)
+const SYSDEF_AF_INET6 = int32(10)
+const SYSDEF_AF_UNIX = int32(1)
+const SYSDEF_MSG_DONTROUTE = int32(4)
+const SYSDEF_MSG_OOB = int32(1)
+const SYSDEF_MSG_PEEK = int32(2)
+const TIME_SPAN_DAY = int64(86400000000)
+const TIME_SPAN_HOUR = int64(3600000000)
+const TIME_SPAN_MILLISECOND = int64(1000)
+const TIME_SPAN_MINUTE = int64(60000000)
+const TIME_SPAN_SECOND = int64(1000000)
+const UNICHAR_MAX_DECOMPOSITION_LENGTH = int32(18)
+
+// Unsupported constant type 'a UTF-8 encoded string' (13) for URI_RESERVED_CHARS_GENERIC_DELIMITERS
+// Unsupported constant type 'a UTF-8 encoded string' (13) for URI_RESERVED_CHARS_SUBCOMPONENT_DELIMITERS
+const USEC_PER_SEC = int32(1000000)
+const VA_COPY_AS_ARRAY = int32(1)
+const VERSION_MIN_REQUIRED = int32(2)
+const WIN32_MSG_HANDLE = int32(19981206)
