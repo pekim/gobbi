83c85fe51b927f12ee945a00f55921e7d53bc6fe
Mike D Pilsbury <mike.pilsbury@gmail.com>
2019-11-10 10:37:06 +0000

    Find all constants in a repo.

--- internal/generate/repository.go
+++ internal/generate/repository.go
@@ -10,40 +10,67 @@ import "C"
 import (
 	"fmt"
 	"os"
-	"unsafe"
 )
 
 type repository struct {
-	name       string
-	version    string
-	format     bool
+	name    string
+	version string
+	format  bool
+
+	cName      *C.char
+	cVersion   *C.char
 	repository *C.GIRepository
 }
 
 func ForRepository(name string, version string, format bool) {
-	repo:= &repository{
-		name:       name,
-		version:    version,
-		format:     format,
+	r := &repository{
+		name:    name,
+		version: version,
+		format:  format,
+
+		cName:      C.CString(name),
+		cVersion:   C.CString(version),
 		repository: C.g_irepository_get_default(),
 	}
 
-	repo.require()
+	r.require()
+	r.generate()
 }
 
-func (r*repository) require() {
-	cName:=C.CString(r.name)
-	defer C.free(unsafe.Pointer(cName))
+// require loads the repo.
+func (r *repository) require() {
+	var err *C.GError
+	C.g_irepository_require(r.repository, r.cName, r.cVersion, 0, &err)
 
-	cVersion:=C.CString(r.version)
-	defer C.free(unsafe.Pointer(cVersion))
-
-	var error *C.GError
-	C.g_irepository_require (r.repository, cName, cVersion, 0, &error);
-
-	if error!=nil {
-		message:=C.GoString(error.message)
-		fmt.Printf("\nFailed to require %s %s, %s\n",r.name, r.version,message)
+	if err != nil {
+		message := C.GoString(err.message)
+		fmt.Printf("\nFailed to require %s %s, %s\n", r.name, r.version, message)
 		os.Exit(1)
 	}
+}
+
+func (r *repository) generate() {
+	r.foreachInfo(C.GI_INFO_TYPE_CONSTANT, r.generateConstant)
+}
+
+func (r *repository) foreachInfo(infoType C.GIInfoType, fn func(*C.GIBaseInfo)) {
+	n := C.g_irepository_get_n_infos(r.repository, r.cName)
+	for i := C.int(0); i < n; i++ {
+		info := C.g_irepository_get_info(r.repository, r.cName, i)
+
+		if C.g_base_info_get_type(info) == infoType {
+			fn(info)
+		}
+	}
+}
+
+func (r *repository) generateConstant(info *C.GIBaseInfo) {
+	cName := C.g_base_info_get_name(info)
+	name := C.GoString(cName)
+
+	constant := (*C.GIConstantInfo)(info)
+	var value C.GIArgument
+	size := C.g_constant_info_get_value(constant, &value)
+
+	fmt.Println(name, size)
 }
