7e0a4b99f6100e4e1c457240ef4aa6bc1670e168
Mike D Pilsbury <mike.pilsbury@gmail.com>
2019-11-12 20:01:00 +0000

    Add a test for atk package, to confirm it builds.

---
+++ internal/test/atk/atk_test.go
@@ -0,0 +1,11 @@
+package atk
+
+import (
+	"github.com/pekim/gobbi/lib/atk"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestCleanBuild(t *testing.T) {
+	assert.Equal(t, int32(2), atk.MajorVersion)
+}
