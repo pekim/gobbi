4ddbcf371617b7287e1d35e4bcc11760b2a21c06
Mike D Pilsbury <mike.pilsbury@gmail.com>
2019-12-18 19:49:27 +0000

    Add temporary test file to test calling sys func.

---
+++ lib/gtk/temp_test.go
@@ -0,0 +1,13 @@
+package gtk
+
+import (
+	"fmt"
+	"github.com/pekim/gobbi/lib/internal/c/gtk"
+	"testing"
+)
+
+func TestFuncCall(t *testing.T) {
+	var x gtk.ToolbarStyle
+	fmt.Println(x)
+	//gtk.Func_gtk_init(x)
+}
