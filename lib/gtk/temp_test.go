2818e7a2880d46fb27ff7f57eac0ada5bb747018
Mike D Pilsbury <mike.pilsbury@gmail.com>
2019-12-26 12:58:29 +0000

    Remove test for gtk_init with no args.
    Can't initialise gtk twice. The second time does nothing, and
    args are not processed.

--- lib/gtk/temp_test.go
+++ lib/gtk/temp_test.go
@@ -1,26 +1,16 @@
 package gtk
 
 import (
-	"fmt"
 	"github.com/pekim/gobbi/lib/internal/c/gtk"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
-func TestStringArrayInoutEmpty(t *testing.T) {
-	args := []string{}
-	var count int = len(args)
-
-	gtk.Fn_gtk_init(&count, &args)
-	assert.Equal(t, 0, count)
-}
-
 func TestStringArrayInout(t *testing.T) {
 	args := []string{"qaz", "--g-fatal-warnings", "qwerty"}
 	var count int = len(args)
-
 	gtk.Fn_gtk_init(&count, &args)
-	fmt.Println(args)
+
 	assert.Equal(t, 2, count)
 	assert.Equal(t, []string{"qaz", "qwerty"}, args)
 }
