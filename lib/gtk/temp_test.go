b8a9a55c37223e06dd67d5e282f4d525ac8c66f2
Mike D Pilsbury <mike.pilsbury@gmail.com>
2019-12-26 14:44:34 +0000

    Move sys gtk test in to sys gtk package.

--- lib/gtk/temp_test.go
+++ lib/gtk/temp_test.go
@@ -1,16 +1,1 @@
 package gtk
-
-import (
-	"github.com/pekim/gobbi/lib/internal/c/gtk"
-	"github.com/stretchr/testify/assert"
-	"testing"
-)
-
-func TestStringArrayInout(t *testing.T) {
-	args := []string{"qaz", "--g-fatal-warnings", "qwerty"}
-	var count int = len(args)
-	gtk.Fn_gtk_init(&count, &args)
-
-	assert.Equal(t, 2, count)
-	assert.Equal(t, []string{"qaz", "qwerty"}, args)
-}
--- lib/internal/c/gtk/sys_test.go
+++ lib/internal/c/gtk/sys_test.go
@@ -1,11 +1,18 @@
 package gtk
 
 import (
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestBuilds(t *testing.T) {
 }
 
-func TestFunctionCall(t *testing.T) {
+func TestStringArrayInout(t *testing.T) {
+	args := []string{"qaz", "--g-fatal-warnings", "qwerty"}
+	var count int = len(args)
+	Fn_gtk_init(&count, &args)
+
+	assert.Equal(t, 2, count)
+	assert.Equal(t, []string{"qaz", "qwerty"}, args)
 }
