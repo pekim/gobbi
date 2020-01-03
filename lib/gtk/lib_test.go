ff6e8517a9ac72a3dba765bb1132653999de1e42
Mike D Pilsbury <mike.pilsbury@gmail.com>
2020-01-03 21:50:48 +0000

    Start to use lib/gtk in example.

--- lib/gtk/lib_test.go
+++ lib/gtk/lib_test.go
@@ -1,1 +1,10 @@
 package gtk
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestFunctionReturningInt(t *testing.T) {
+	assert.Equal(t, uint(3), GetMajorVersion())
+}
--- lib/internal/example/sys_window_simple/main.go
+++ lib/internal/example/sys_window_simple/main.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"github.com/pekim/gobbi/lib/internal/c/gtk"
+	"github.com/pekim/gobbi/lib/gtk"
+	gtkSys "github.com/pekim/gobbi/lib/internal/c/gtk"
 	"github.com/pekim/gobbi/lib/internal/c/signal"
 	"os"
 	"runtime"
@@ -14,16 +15,16 @@ func init() {
 func main() {
 	argv := os.Args
 	var argc int = len(argv)
-	gtk.Fn_gtk_init(&argc, &argv)
+	gtkSys.Fn_gtk_init(&argc, &argv)
 
-	window := gtk.Fn_gtk_window_new(0)
-	gtk.Fn_gtk_window_set_title(window, "some title")
-	gtk.Fn_gtk_window_set_default_size(window, 400, 300)
-	gtk.Fn_gtk_widget_show_all(window)
+	window := gtkSys.Fn_gtk_window_new(0)
+	gtkSys.Fn_gtk_window_set_title(window, "some title")
+	gtkSys.Fn_gtk_window_set_default_size(window, 400, 300)
+	gtkSys.Fn_gtk_widget_show_all(window)
 
 	signal.ConnectSignal(window, "destroy", func(return_value *signal.Value, paramValues []signal.Value) {
-		gtk.Fn_gtk_main_quit()
+		gtk.MainQuit()
 	})
 
-	gtk.Fn_gtk_main()
+	gtk.Main()
 }
