a6d86c9192942b6564efe6bd3cf8580832f7773a
Mike D Pilsbury <mike.pilsbury@gmail.com>
2020-02-08 11:01:18 +0000

    Default error handling to panic.

--- error-handling.go
+++ error-handling.go
@@ -2,17 +2,26 @@ package gobbi
 
 import "github.com/pekim/gobbi/internal/cgo/gi"
 
+func init() {
+	SetErrorHandler(func(err error) {
+		panic(err)
+	})
+}
+
 /*
 	SetErrorHandler sets a handler function that will be called
-	if an error occurs dynamically referencing an entity while
-	calling a function.
+	if an error occurs while calling a function.
 
 	Such errors may occur for a number of reasons.
 	A library is not installed.
 	A function is not available in the installed version of a library.
 	A record or class is not available in the installed version of a library.
 
-	Pass nil to remove a previous set handler.*/
+	Pass nil to remove a previous set handler.
+
+	If no error handler is set,
+	the default handler will panic when an error occurs.
+*/
 func SetErrorHandler(handler gi.ErrorHandler) {
 	gi.SetErrorHandler(handler)
 }
--- internal/cgo/gi/function_test.go
+++ internal/cgo/gi/function_test.go
@@ -2,11 +2,28 @@ package gi
 
 import (
 	"github.com/stretchr/testify/assert"
+	"os"
 	"testing"
 )
 
-func TestFunctionIntegerReturn(t *testing.T) {
+func TestMain(m *testing.M) {
+	SetErrorHandler(func(err error) {
+		panic(err)
+	})
+
 	Require("GLib", "2.0")
+
+	os.Exit(m.Run())
+}
+
+func TestFunctionIntegerReturn(t *testing.T) {
+	fn, _ := Function2InvokerNew("GLib", "get_num_processors")
+
+	ret := fn.Invoke([]Arg{}, 0, 0, Arg{typ: ArgType_uint})
+	assert.True(t, ret.value.(uint) > 0)
+}
+
+func TestFunctionIntegerReturn2(t *testing.T) {
 	fn, _ := Function2InvokerNew("GLib", "get_num_processors")
 
 	ret := fn.Invoke([]Arg{}, 0, 0, Arg{typ: ArgType_uint})
