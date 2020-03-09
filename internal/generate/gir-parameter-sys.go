f01a6fb42000af9289be7ef76bc0c3c076e18ba0
Mike D Pilsbury <mike.pilsbury@gmail.com>
2020-03-09 17:29:06 +0000

    Move sys func param declaration to its own file.

--- internal/generate/gir-parameter-sys.go
+++ internal/generate/gir-parameter-sys.go
@@ -5,38 +5,6 @@ import (
 	"github.com/dave/jennifer/jen"
 )
 
-func (p *Parameter) sysParamGoType() *jen.Statement {
-	if p.isType() {
-		// Atoms are really pointers underneath.
-		if p.Type.CType == "GdkAtom" {
-			return jenUnsafePointer()
-		}
-
-		star := ""
-		if p.Nullable && !p.isOut() && !p.Type.isStruct() && !p.Type.isPointer() {
-			// nullable simple type, so make it a pointer
-			star = "*"
-		}
-
-		return jen.
-			Op(star).
-			Add(p.Type.sysParamGoType())
-	}
-
-	if p.isArray() {
-		star := ""
-		if p.isOut() {
-			star = "*"
-		}
-
-		return jen.
-			Op(star).
-			Add(p.Array.sysParamGoType())
-	}
-
-	panic(fmt.Sprintf("Parameter is not a type or an array: %s", p.context))
-}
-
 func (p *Parameter) generateSysCArg(g *jen.Group) {
 	if p.isType() && p.Nullable && !p.isOut() && !p.Type.isStruct() && !p.Type.isPointer() {
 		p.generateSysCArgNullable(g, p.cVarName)
